70a7eaefb82ace688648ca25e5e74d5d3b2eb00b
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-10-06 09:56:53 -0700

    Chapter 2 lab 1 Day 2

---
+++ LuckyNumbers.java
@@ -0,0 +1,27 @@
+// **************************************************
+//   LuckyNumbers.java
+//   Author: A. Starr
+//   Date: 10/6/17
+//   To generate two random "lucky" numbers
+// **************************************************
+ 
+import java.util.Random;
+ 
+public class LuckyNumbers
+{
+   public static void main (String[] args)
+   {
+          Random generator = new Random();
+          int lucky1, lucky2; 
+ 
+          // Generate lucky1 (a random integer between 50 and 79) using the nextInt method
+ 
+          lucky1 = ______________________________________________________;
+ 
+          // Generate lucky2 (a random integer between 11 and 30) using nextFloat
+ 
+          lucky2 = ______________________________________________________;
+ 
+          System.out.println ("Your lucky numbers are " + lucky1 + " and " + lucky2);
+        }
+}
---
+++ Paint.java
@@ -0,0 +1,37 @@
+package Chapter2;
+
+//***************************************************************
+//File: Paint.java
+//Author: A. Starr
+//Date: 10/6/17
+//Purpose: Determine how much paint is needed to paint the walls
+//of a room given its length, width, and height
+//***************************************************************
+ 
+public class Paint
+{
+        public static void main(String[] args)
+        {
+            final int COVERAGE = 350;          //paint covers 350 sq ft/gal
+            int length, width, height;         //declare integers length, width, and height;
+            double totalSqFt;                  //declare double totalSqFt;
+            double paintNeeded;                //declare double paintNeeded;
+ 
+            //Declare and initialize the length, width, and height of the room
+            length = 15;
+            width = 11;
+            height = 8;
+            
+            //Compute the total square feet to be painted--think
+                        //about the dimensions of each wall
+            totalSqFt = ((height*width*4) + (length*width));
+            
+            //Compute the amount of paint needed
+            paintNeeded = (int) (Math.ceil(totalSqFt/ 350));
+            System.out.println ("You need " + paintNeeded + " gallons.");
+ 
+            //Print the length, width, and height of the room and the
+            //number of gallons of paint needed
+            System.out.println (("height = " + height)+ ("\nlength = " + length) + ("\nwidth = " + width) +("\nNumber of gallons of paint needed = " + paintNeeded));
+        }
+}
