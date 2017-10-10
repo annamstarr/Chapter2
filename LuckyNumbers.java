1a85ec33150c86ff6063a9f5c91d3f4b9562dc34
jhstarr <jeffhstarr@gmail.com>
2017-10-09 21:14:34 -0700

    Done

--- LuckyNumbers.java
+++ LuckyNumbers.java
@@ -16,11 +16,11 @@ public class LuckyNumbers
  
           // Generate lucky1 (a random integer between 50 and 79) using the nextInt method
  
-          lucky1 = ______________________________________________________;
+          lucky1 = Math.abs(generator.nextInt()) % 40 + 50;
  
           // Generate lucky2 (a random integer between 11 and 30) using nextFloat
  
-          lucky2 = ______________________________________________________;
+          lucky2 = (int) (generator.nextFloat() * 21) + 11;
  
           System.out.println ("Your lucky numbers are " + lucky1 + " and " + lucky2);
         }
